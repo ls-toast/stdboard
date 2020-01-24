35f3a970b92e507a8f376ef9a60019f6c48c972d
mac-toast <leessak2004@gmail.com>
2020-01-24 08:16:05 +0000

    Add register code

--- reg.php
+++ reg.php
@@ -5,5 +5,13 @@ $pw = $_GET['password'];
 $name = $_GET['name'];
 $no_qry = "select max(no) from users";
 $res = mysqli_query($con, $no_qry);
-echo $res;
+$row = mysqli_fetch_array($res);
+$no = $row['max(no)'];
+$no = $no+1;
+$insert_qry = "insert into users(no,id,pw,name) values($no,'{$id}','{$pw}','{$name}')";
+$res = mysqli_query($con, $insert_qry);
+if($res)
+ echo "INSERT SUCCESSS";
+else
+ echo "INSERT ERROR";
 ?>
