c4af1bd019589fd9715c7e69de1103e10164b79a
ls-toast <leesak2004@gmail.com>
2020-01-25 17:13:44 +0900

    Add login_check code

--- login.php
+++ login.php
@@ -1,9 +1,17 @@
 <?php
 include "./common.php";
-if($con)
- echo "CONNECTED!";
-else
+if(!$con)
  echo "FAILED!";
-echo $_GET['id'];
-echo $_GET['password'];
+$id = $_GET['id'];
+$pw = $_GET['password'];
+
+$chk_qry="SELECT * FROM users where id='$id'";
+$res=mysqli_query($con, $chk_qry);
+if(!$res)
+    echo "ID ERROR";
+$row=mysqli_fetch_array($res);
+if($pw == $row['pw'])
+    echo "LOGIN SUCCESS!";
+else
+    echo "PASSWORD ERROR!";
 ?>
