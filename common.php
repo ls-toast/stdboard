065b9afe34f20f33a13fabb28c2cc5c17aaad6ec
mac-toast <leessak2004@gmail.com>
2020-01-26 08:21:34 +0000

    modify some code

--- common.php
+++ common.php
@@ -1,3 +1,5 @@
 <?php
 $con = mysqli_connect("localhost","root","toor","stdboard");
+if(!$con)
+ echo "DB CONNECT FAILED!";
 ?>
--- login.php
+++ login.php
@@ -1,18 +1,29 @@
 <?php
+session_start();
 include "./common.php";
-if(!$con)
- echo "FAILED!";
+
 $id = $_GET['id'];
 $pw = $_GET['password'];
 
 $chk_qry="SELECT * FROM users where id='$id'";
 $res=mysqli_query($con, $chk_qry);
-if(!$res)
-    echo "ID ERROR";
 $row=mysqli_fetch_array($res);
+if($id != $row['ID'])
+{
+    echo "<script> alert(\"ID ERROR!\");";
+    echo "location.href='./main.html';</script>";
+}
 
 if($pw == $row['PW'])
-    echo "LOGIN SUCCESS!";
+{
+    $_SESSION['id']=$id;
+    echo "<script> alert(\"HELLO ";
+    echo $id;
+    echo "\")</script>";
+}
 else
-    echo "PASSWORD ERROR!";
+{
+echo "<script> alert(\"PASSWORD ERROR!\");";
+echo "location.href='./main.html';</script>";
+}
 ?>
